1bee4933f6b6f5d25253722afcf56a4adfd5e84a
Andriy Byelikov <andriybyelikov@gmail.com>
2025-04-07 14:42:38 +0200

    Implement main scene placeholder and New Game button action

--- src/scenes/title/title.tscn.gd
+++ src/scenes/title/title.tscn.gd
@@ -5,6 +5,11 @@ func _ready() -> void:
     await SceneTransition.fade_in()
 
 
+func _on_new_game_pressed() -> void:
+    await SceneTransition.fade_out()
+    self.get_tree().change_scene_to_file("res://src/scenes/main/main.tscn")
+
+
 func _on_options_pressed() -> void:
     Options.show()
 
